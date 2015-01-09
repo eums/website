0df40bad44ad2cc0934da253a0eb3934602209cd
Harry Garrood <harry@garrood.me>
2015-01-09 02:53:14 +0000

    Start to promise-ify.

--- _assets/javascripts/jekyll-lunr-search.js
+++ _assets/javascripts/jekyll-lunr-search.js
@@ -146,82 +146,107 @@ function buildSearchDatabase(data, callback) {
   asyncMap(data, addToIndex, done)
 }
 
-// Perform an AJAX request, attempt to parse the returned JSON, and call the
-// callback with the result.
-function getJSON(url, ifModifiedSince, onOk, onNotModified) {
-  var request = new XMLHttpRequest()
-  request.open('GET', url, true)
-  if (ifModifiedSince) {
-    request.setRequestHeader('If-Modified-Since', ifModifiedSince)
-  }
+// Perform an AJAX request for the given url, set the If-Modified-Since header
+// with the given date, and attempt to parse the returned JSON.
+//
+// Returns a promise that will resolve to either:
+//
+//   { state: 'not-modified' }
+//
+// meaning that the resource has not been modified since the supplied time (so
+// you can use the cached value), or:
+//
+//   { state: 'ok', lastModified: String, value: Object }
+//
+function getJSON(url, ifModifiedSince) {
+  return new Promise(function(resolve, reject) {
+    var request = new XMLHttpRequest()
+    request.open('GET', url, true)
 
-  request.onreadystatechange = function() {
-    if (this.readyState !== 4) {
-      return
+    if (ifModifiedSince) {
+      request.setRequestHeader('If-Modified-Since', ifModifiedSince)
     }
 
-    if (this.status === 304) {
-      console.log('getJSON: 304 Not Modified')
-      onNotModified()
-    } else if (this.status >= 200 && this.status < 400) {
-      var lastModified = this.getResponseHeader('Last-Modified')
-      if (ifModifiedSince === lastModified) {
-        console.log('getJSON: 200 OK but it wasn\'t modified')
-        onNotModified()
-      } else {
-        console.log('getJSON: 200 OK')
-        onOk(lastModified, JSON.parse(this.responseText))
+    request.onreadystatechange = function() {
+      if (this.readyState !== 4) {
+        return
       }
-    } else {
-      throw new Error(
-        'getJSON failed. HTTP status was: ' + this.status)
-    }
-  }
 
-  request.send()
-  request = null
+      if (this.status === 304) {
+        console.log('getJSON: 304 Not Modified')
+        resolve({ state: 'not-modified' })
+
+      } else if (this.status >= 200 && this.status < 400) {
+        var lastModified = this.getResponseHeader('Last-Modified')
+
+        if (ifModifiedSince === lastModified) {
+          console.log('getJSON: 200 OK but it wasn\'t modified')
+          resolve({ state: 'not-modified' })
+
+        } else {
+          console.log('getJSON: 200 OK')
+          resolve({
+            state: 'ok',
+            lastModified: lastModified,
+            value: JSON.parse(this.responseText)
+          })
+        }
+      } else {
+        reject(Error('getJSON: HTTP error. Status: ' + this.status))
+      }
+    }
+
+    request.onerror = function() {
+      reject(Error('getJSON: XMLHttpRequest error'))
+    }
+
+    request.send()
+    request = null
+  })
 }
 
 // Given:
 //
 // * an arbitrary caching mechanism that can store a single value
-// * A URL pointing to a JSON object, that might change over time
+// * a URL which points to some JSON to be fetched via AJAX
 // * an expensive computation that always gives the same result for the same
 //   input
-// * a callback that should eventually be called with the result
 //
-// Then use the HTTP Last-Modified / If-Modified-Since mechanism in order to
-// only perform the expensive computation on the object when it has changed.
-function getJSONWithCache(cache, url, expensiveFn, callback) {
-  var cached = cache.get()
-  var ifModifiedSince = cached && cached.lastModified
+// return a new promise which will resolve to the result of running the
+// expensive computation on the promised value, while making use of the
+// provided caching system.
+function getJSONWithCache(cache, url, expensiveFn) {
+  return new Promise(function(resolve, reject) {
+    var cached = cache.get()
+    var ifModifiedSince = cached && cached.lastModified
 
-  var onOk = function(lastModified, responseObject) {
-    nextTick(function() {
-      expensiveFn(responseObject, function(result) {
-        setTimeout(function() {
-          // this is expensive because of the lunr index
-          // serialization, so do it a second later
-          console.log('storing result in cache...')
-          cache.set({ lastModified: lastModified, value: result })
-          console.log('result stored in cache.')
-        }, 1000)
-        callback(result)
-      })
+    getJSON(url, ifModifiedSince).then(function(input) {
+      if (input.state === 'ok') {
+        expensiveFn(input.value, function(result) {
+          setTimeout(function() {
+            // this is expensive-ish because of the lunr index
+            // serialization, so do it a second later
+            console.log('storing result in cache...')
+            cache.set({
+              lastModified: input.lastModified,
+              value: result
+            })
+            console.log('result stored in cache.')
+          }, 1000)
+
+          resolve(result)
+        })
+      } else if (input.state === 'not-modified') {
+        console.log('getJSONWithCache: cache hit!')
+        resolve(cached.value)
+      }
     })
-  }
-
-  var onNotModified = function() {
-    console.log('getJSONWithCache: cache hit!')
-    callback(cached.value)
-  }
-
-  getJSON(url, ifModifiedSince, onOk, onNotModified)
+  })
 }
 
 // Now with caching!
-function getSearchDatabase(url, callback) {
-  getJSONWithCache(databaseCache, url, buildSearchDatabase, callback)
+function getSearchDatabase(url) {
+  return getJSONWithCache(databaseCache, url, buildSearchDatabase)
 }
 
 function createLunrIndex() {
@@ -324,7 +349,7 @@ function main(config) {
     setSearchHeaderText(config.headerId, query)
 
     retry(1000, 10, function(done) {
-      getSearchDatabase(config.jsonUrl, function(db) {
+      getSearchDatabase(config.jsonUrl).then(function(db) {
         runQuery(db, query, function(results) {
           displayResults(config.template, config.containerId, results)
           done()
